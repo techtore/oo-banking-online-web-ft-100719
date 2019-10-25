1f1a60dde15c7b74ffa1877fd00c95c2b44d4654
Victoria Hale <vic.hale74@gmail.com>
2019-10-25 04:12:10 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -10,7 +10,7 @@ class Transfer
   end
 
   def valid?
-    if @sender.valid? && @receiver.valid?
+    if sender.valid? && receiver.valid?
       true 
     else
       false
@@ -18,16 +18,16 @@ class Transfer
   end
 
   def execute_transaction
-    if valid? && @sender.balance > @amount && @status = "pending"
-      @sender.balance -= @amount
-      @receiver.balance += @amount
-      @status = "complete"
+    if valid? && sender.balance > amount && status = "pending"
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
     else 
-      @status = "rejected"
+      self.status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
     
   end
-  @status
+  # status
 end
 
