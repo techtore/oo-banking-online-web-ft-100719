64febd4338df29c644c59033272a85579586510d
Victoria Hale <vic.hale74@gmail.com>
2019-10-25 03:45:27 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ class Transfer
   def execute_transaction
     if @sender.balance > @amount && @status = "pending"
       @sender.balance -= @amount 
-      @receiver.deposit(@amount)
+      @receiver.balance += @amount
       @status = "complete"
     else 
       @status = "rejected"
