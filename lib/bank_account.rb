0e016489cbfe993609a22d5393ca2e7bb722e367
Victoria Hale <vic.hale74@gmail.com>
2019-10-25 03:54:26 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -18,11 +18,8 @@ class BankAccount
   end
   
   def valid?
-   if @status == "open" && @balance > 0
-     true 
-   else
-     false 
-   end
+    @status = "open" && @balance > 0 ? true : false
+   
   end
   
   def close_account
