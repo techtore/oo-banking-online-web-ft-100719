33faf48ee6964b46728ed1e1370325ce8bb32304
Victoria Hale <vic.hale74@gmail.com>
2019-10-25 04:30:10 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -35,16 +35,15 @@ class Transfer
     if valid? && receiver.balance > amount && self.status == "complete" 
        receiver.balance -= amount
        sender.balance += amount
-      
-      # sender.deposit(@@TRANSACTION_HISTORY.last)
-      # receiver.balance -= @@TRANSACTION_HISTORY.last 
-      # @@TRANSACTION_HISTORY.delete(@@TRANSACTION_HISTORY.last)
-      self.status = "reversed"
+       self.status = "reversed"
     else 
       self.status = "rejected"
       "Transaction rejected. Please check your account balance."
     end
   end
- 
 end
+
+      # sender.deposit(@@TRANSACTION_HISTORY.last)
+      # receiver.balance -= @@TRANSACTION_HISTORY.last 
+      # @@TRANSACTION_HISTORY.delete(@@TRANSACTION_HISTORY.last)
 
